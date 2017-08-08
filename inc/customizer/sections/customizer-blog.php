a4575a06003162cae79132097dc8a882a0afa724
Thomas W <kontakt@themecoder.de>
2017-08-08 11:14:21 +0200

    added new setting to deactivate Magazine widgets on blog index

--- inc/customizer/sections/customizer-blog.php
+++ inc/customizer/sections/customizer-blog.php
@@ -52,5 +52,31 @@ function treville_customize_register_blog_settings( $wp_customize ) {
 		'type'     => 'textarea',
 		'priority' => 20,
 	) );
+
+	// Add Magazine Widgets Headline.
+	$wp_customize->add_control( new Treville_Customize_Header_Control(
+		$wp_customize, 'treville_theme_options[blog_magazine_widgets_title]', array(
+			'label'    => esc_html__( 'Magazine Widgets', 'treville' ),
+			'section'  => 'treville_section_blog',
+			'settings' => array(),
+			'priority' => 30,
+		)
+	) );
+
+	// Add Setting and Control for Magazine widgets.
+	$wp_customize->add_setting( 'treville_theme_options[blog_magazine_widgets]', array(
+		'default'           => true,
+		'type'              => 'option',
+		'transport'         => 'refresh',
+		'sanitize_callback' => 'treville_sanitize_checkbox',
+	) );
+
+	$wp_customize->add_control( 'treville_theme_options[blog_magazine_widgets]', array(
+		'label'    => esc_html__( 'Display Magazine widgets on blog index', 'treville' ),
+		'section'  => 'treville_section_blog',
+		'settings' => 'treville_theme_options[blog_magazine_widgets]',
+		'type'     => 'checkbox',
+		'priority' => 40,
+	) );
 }
 add_action( 'customize_register', 'treville_customize_register_blog_settings' );
