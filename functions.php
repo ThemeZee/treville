ae1802e579935c1147ed3fe7f8f02eab2fb5ee19
Thomas W <kontakt@themecoder.de>
2018-04-13 15:55:41 +0200

    added local fonts

--- functions.php
+++ functions.php
@@ -73,7 +73,7 @@ if ( ! function_exists( 'treville_setup' ) ) :
 		) ) );
 
 		// Add extra theme styling to the visual editor.
-		add_editor_style( array( 'css/editor-style.css', treville_google_fonts_url() ) );
+		add_editor_style( array( 'css/editor-style.css', get_template_directory_uri() . '/assets/css/custom-fonts.css' ) );
 
 		// Add Theme Support for Selective Refresh in Customizer.
 		add_theme_support( 'customize-selective-refresh-widgets' );
@@ -146,9 +146,6 @@ function treville_scripts() {
 	// Register and enqueue navigation.js.
 	wp_enqueue_script( 'treville-jquery-navigation', get_template_directory_uri() . '/js/navigation.js', array( 'jquery' ), '20170619' );
 
-	// Register and Enqueue Google Fonts.
-	wp_enqueue_style( 'treville-default-fonts', treville_google_fonts_url(), array(), null );
-
 	// Register Comment Reply Script for Threaded Comments.
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -158,22 +155,15 @@ add_action( 'wp_enqueue_scripts', 'treville_scripts' );
 
 
 /**
- * Retrieve Font URL to register default Google Fonts
+ * Enqueue custom fonts.
  */
-function treville_google_fonts_url() {
+function treville_custom_fonts() {
 
-	// Set default Fonts.
-	$font_families = array( 'Gudea:400,400italic,700,700italic', 'Magra:400,400italic,700,700italic' );
+	// Register and Enqueue Theme Fonts.
+	wp_enqueue_style( 'treville-custom-fonts', get_template_directory_uri() . '/assets/css/custom-fonts.css', array(), '20180413' );
 
-	// Build Fonts URL.
-	$query_args = array(
-		'family' => urlencode( implode( '|', $font_families ) ),
-		'subset' => urlencode( 'latin,latin-ext' ),
-	);
-	$fonts_url = add_query_arg( $query_args, '//fonts.googleapis.com/css' );
-
-	return apply_filters( 'treville_google_fonts_url', $fonts_url );
 }
+add_action( 'wp_enqueue_scripts', 'treville_custom_fonts', 1 );
 
 
 /**
