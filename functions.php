923c1f95959afafb27da4f875485a5f51c1bbc41
Thomas Weichselbaumer <kontakt@themecoder.de>
2020-08-21 12:06:46 +0200

    added svgxuse

--- functions.php
+++ functions.php
@@ -188,6 +188,9 @@ function treville_scripts() {
 		wp_localize_script( 'treville-navigation', 'trevilleScreenReaderText', $treville_l10n );
 	}
 
+	// Enqueue svgxuse to support external SVG Sprites in Internet Explorer.
+	wp_enqueue_script( 'svgxuse', get_theme_file_uri( '/assets/js/svgxuse.min.js' ), array(), '1.2.6' );
+
 	// Register Comment Reply Script for Threaded Comments.
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
