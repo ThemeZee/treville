6fbcb94fe764a3f31c075b8620265f5be34fca51
Thomas Weichselbaumer <kontakt@themecoder.de>
2021-05-03 18:10:01 +0200

    fixed typo in pagination function

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -393,7 +393,7 @@ if ( ! function_exists( 'treville_pagination' ) ) :
 
 		the_posts_pagination( array(
 			'mid_size'  => 2,
-			'prev_text' => '&laquo<span class="screen-reader-text">' . esc_html_x( 'Previous Posts', 'pagination', 'treville' ) . '</span>',
+			'prev_text' => '&laquo;<span class="screen-reader-text">' . esc_html_x( 'Previous Posts', 'pagination', 'treville' ) . '</span>',
 			'next_text' => '<span class="screen-reader-text">' . esc_html_x( 'Next Posts', 'pagination', 'treville' ) . '</span>&raquo;',
 		) );
 
